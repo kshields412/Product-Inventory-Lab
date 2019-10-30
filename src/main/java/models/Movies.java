2a99756cb20c8d02c1defea2046ede7eb2c40678
Kai Shields <kai@zipcoders-MacBook-Pro-3.local>
2019-10-30 17:47:48 -0400

    added properties of Movies

--- src/main/java/models/Movies.java
+++ src/main/java/models/Movies.java
@@ -1,4 +1,9 @@
 package models;
 
 public class Movies {
+    private int runTime;
+    private String genre;
+    private float ticketPrice;
+    private String title;
+    private float openingWeekSales;
 }
